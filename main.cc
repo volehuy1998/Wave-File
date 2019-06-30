a6f92a66e2918e3fb579772e133878569414d5ee
Zhang Liang <36606761+volehuy1998@users.noreply.github.com>
2019-06-30 22:43:50 +0700

    Copy wav success
    Read origin file and write all to out

--- main.cc
+++ main.cc
@@ -1,16 +1,24 @@
-#include <fstream>
 #include <iostream>
+#include <fstream>
+#include <cstring>
 
 using namespace std;
 using byte = char;
 
-const char *path = "audio.wav";
+const char *pathIn =  "audio.wav";
+const char *pathOut = "audio2.wav";
 
 struct Wav_RIFF_header
 {
 	char ChunkID[4];
 	char ChunkSize[4];
 	char Format[4];
+	Wav_RIFF_header()
+	{
+		memset(ChunkID, 0, sizeof ChunkID);
+		memset(ChunkSize, 0, sizeof ChunkSize);
+		memset(Format, 0, sizeof Format);
+	}
 } riff;
 
 struct Wav_FMT
@@ -24,28 +32,64 @@ struct Wav_FMT
 	char BlockAlign[2];
 	char BitsPerSample[2];
 	char ExtraParamSize[2];
+	Wav_FMT()
+	{
+		memset(Subchunk1ID, 0, sizeof Subchunk1ID);
+		memset(Subchunk1Size, 0, sizeof Subchunk1Size);
+		memset(AudioFormat, 0, sizeof AudioFormat);
+		memset(NumChannels, 0, sizeof NumChannels);
+		memset(SampleRate, 0, sizeof SampleRate);
+		memset(ByteRate, 0, sizeof ByteRate);
+		memset(BlockAlign, 0, sizeof BlockAlign);
+		memset(BitsPerSample, 0, sizeof BitsPerSample);
+		memset(ExtraParamSize, 0, sizeof ExtraParamSize);
+	}
 } fmt;
 
 struct Wav_DATA_header
 {
 	char Subchunk2ID[4];
 	char Subchunk2Size[4];
+	Wav_DATA_header()
+	{
+		memset(Subchunk2ID, 0, sizeof Subchunk2ID);
+		memset(Subchunk2Size, 0, sizeof Subchunk2Size);
+	}
 } data_header;
 
-struct Wav_Audio
+struct Wav_audio
 {
 	char* data;
-} audio;
+} data;
 
 int main()
 {
-	ifstream is (path, ios::binary);
-	
+	ifstream is (pathIn,  ios::binary);
+	ofstream os (pathOut, ios::binary); 
+	// Read header
 	is.read((char*)&riff, sizeof riff);
 	is.read((char*)&fmt, sizeof fmt);
 	is.read((char*)&data_header, sizeof data_header);
 
+	// Get length
+	int len = *(int*)data_header.Subchunk2Size;
+
+	// Alloc
+	data.data = new char[len];
+
+	// Read data
+	is.read(data.data, len);
+	
+	// Write data
+	os.write((char*)&riff, sizeof riff);
+	os.write((char*)&fmt, sizeof fmt);
+	os.write((char*)&data_header, sizeof data_header);
+	os.write(data.data, len);
+
+	// Fin
+	delete[] data.data;
 	is.close();
+	os.close();
 	return 0;
 }
 
